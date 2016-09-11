48f453298a2c0344efb547e71820276d0cb68a9e
Coen Hyde <coen.hyde@gmail.com>
2016-09-11 14:19:08 -0700

    write tests for validator filter

--- test/validator/filter.js
+++ test/validator/filter.js
@@ -3,35 +3,33 @@
 var expect = require('expect.js');
 var validator = require('../../lib/validator');
 
-describe('Validator:Defaults', function() {
-  it('should apply defaults to flat schema', function(done) {
+describe('Validator:Filter', function() {
+  it('should remove unlisted properties from flat schema', function(done) {
     var schema = {
       name: 'test',
       properties: {
         key1: {
           description: 'key1',
           type: 'string',
-          default: 'boop',
-          required: true
         },
         key2: {
           description: 'key1',
           type: 'string',
-          default: 'applied default',
-          required: false
         }
       }
     }
 
     var data = {
-      key1: "value1"
+      key1: "value1",
+      key3: "yo"
     }
 
     validator(data, schema, function(err, result, data) {
       expect(err).to.equal(null);
       expect(result.valid).to.equal(true);
-      expect(data.key1).to.not.equal('boop');
-      expect(data.key2).to.equal('applied default');
+
+      // Key 3 should not exist
+      expect(data.key3).to.equal(undefined);
       done();
     });
   });
@@ -43,19 +41,13 @@ describe('Validator:Defaults', function() {
         key1: {
           description: 'key1',
           type: 'string',
-          required: true
         },
         nested: {
           description: 'Nested values',
           type: 'object',
           properties: {
             key1: {
-              type: 'string',
-              default: 'yo'
-            },
-            key2: {
-              type: 'string',
-              default: 'beep'
+              type: 'string'
             }
           }
         }
@@ -72,8 +64,7 @@ describe('Validator:Defaults', function() {
     validator(data, schema, function(err, result, data) {
       expect(err).to.equal(null);
       expect(result.valid).to.equal(true);
-      expect(data.nested.key1).to.equal('yo');
-      expect(data.nested.key2).to.not.equal('beep');
+      expect(data.nested.key2).to.equal(undefined);
       done();
     });
   });
