4dbe7fe5a732624486746163ac3c64d31daaa909
Coen Hyde <coen.hyde@gmail.com>
2017-07-03 20:51:06 +1000

    adding dynamodb put

--- test/adapters/dynamodb.js
+++ test/adapters/dynamodb.js
@@ -1,65 +1,43 @@
 'use strict'
 
-var AWS = require('aws-sdk-mock');
+var awsMock = require('aws-sdk-mock');
+var AWS = require('aws-sdk');
 var expect = require('expect.js');
+var bluey = require('../../');
 var adapter = require('../../lib/adapters/dynamodb');
 
 describe('Adapter:Dynamodb', function() {
-  describe('DataTransform', function() {
-    var tests = [
-      { type: 'string', dynoType: 'S', data: 'foo' },
-      { type: 'iteger', dynoType: 'N', data: 1 },
-      { type: 'boolean', dynoType: 'BOOL', data: true },
-      { type: 'array', dynoType: 'L', data: [1,2,3] }
-    ];
+  afterEach(() => awsMock.restore());
 
-    tests.forEach((test) => {
-      it(`should map ${test.type} columns`, function(done) {
-        var properties = {
-          key: {
-            type: test.type
-          }
-        }
+  it('should put an item', function(done) {
+    var data = { id: 'test', name: 'test' };
 
-        var data = { key: test.data };
-        var transformation = adapter._dataToItem(properties, data);
+    awsMock.mock('DynamoDB.DocumentClient', 'put', function (params, cb){
+      expect(params.Item.id).to.eql(data.id);
+      expect(params.Item.name).to.eql(data.name);
 
-        expect(transformation).to.eql({
-          key: {
-            [test.dynoType]: test.data
-          }
-        });
-
-        done();
-      });
+      cb(null, "successfully put item in database");
     });
 
-    it('should map nested data', function(done) {
-      var properties = {
-        key: {
-          type: 'object',
-          properties: {
-            nested: {
-              type: 'string'
-            }
-          }
+    var model = bluey({
+      name: 'users',
+      adapter: new AWS.DynamoDB(),
+      type: 'dynamodb',
+      properties: {
+        id: {
+          type: 'string',
+          required: true
+        },
+        name: {
+          type: 'string',
+          required: true
         }
       }
+    });
 
-      var data = { key: { nested: 'value' } };
-      var transformation = adapter._dataToItem(properties, data);
-
-      expect(transformation).to.eql({
-        key: {
-          M: {
-            nested: {
-              S: 'value'
-            }
-          }
-        }
-      });
-
+    model.put({id: 'test', name: 'test'}, (err, data) => {
+      expect(err).to.equal(null);
       done();
     });
-  })
+  });
 });
