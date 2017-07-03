4efe5dbbcb3f7c2576d7340d988f8651bc30a0bc
Coen Hyde <coen.hyde@gmail.com>
2017-07-03 21:50:49 +1000

    adding get to dynamodb adapter

--- test/adapters/dynamodb.js
+++ test/adapters/dynamodb.js
@@ -29,8 +29,8 @@ describe('Adapter:Dynamodb', function() {
     var data = { id: 'test', name: 'test' };
 
     awsMock.mock('DynamoDB.DocumentClient', 'put', function (params, cb){
-      expect(params.Item.id).to.eql(data.id);
-      expect(params.Item.name).to.eql(data.name);
+      expect(params.Item.id).to.equal(data.id);
+      expect(params.Item.name).to.equal(data.name);
 
       cb(null, "successfully put item in database");
     });
@@ -43,13 +43,28 @@ describe('Adapter:Dynamodb', function() {
 
   it('should delete an item', function(done) {
     awsMock.mock('DynamoDB.DocumentClient', 'delete', function (params, cb){
-      expect(params.Key.HashKey).to.eql('uuid');
-      expect(params.Key.NumberRangeKey).to.eql('yo');
+      expect(params.Key.HashKey).to.equal('uuid');
+      expect(params.Key.NumberRangeKey).to.equal('yo');
       cb(null);
     });
 
     model.del('yo', (err, data) => {
       expect(err).to.equal(null);
+      done();
+    });
+  });
+
+  it('should get an item', function(done) {
+    var item = { uuid: 'yo', key: 'value' };
+
+    awsMock.mock('DynamoDB.DocumentClient', 'get', function (params, cb){
+      expect(params.Key.uuid).to.equal('yo');
+      cb(null, { Item: item });
+    });
+
+    model.get('yo', (err, item) => {
+      expect(err).to.equal(null);
+      expect(item).to.equal(item);
       done();
     });
   });
