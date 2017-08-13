d0007e891c5ea445fba1d8c582af663269125f85
Coen Hyde <coen.hyde@gmail.com>
2017-08-13 14:23:02 -0700

    add PartialPut to dynamodb adapter

--- test/adapters/dynamodb.js
+++ test/adapters/dynamodb.js
@@ -21,8 +21,26 @@ describe('Adapter:Dynamodb', function() {
       name: {
         type: 'string',
         required: true
+      },
+      age: {
+        type: 'number'
       }
     }
+  });
+
+  it('should get an item', function(done) {
+    var item = { uuid: 'yo', key: 'value' };
+
+    awsMock.mock('DynamoDB.DocumentClient', 'get', function (params, cb){
+      expect(params.Key.uuid).to.equal('yo');
+      cb(null, { Item: item });
+    });
+
+    model.get('yo', (err, item) => {
+      expect(err).to.equal(null);
+      expect(item).to.equal(item);
+      done();
+    });
   });
 
   it('should put an item', function(done) {
@@ -32,10 +50,38 @@ describe('Adapter:Dynamodb', function() {
       expect(params.Item.id).to.equal(data.id);
       expect(params.Item.name).to.equal(data.name);
 
-      cb(null, "successfully put item in database");
+      cb(null, { Attributes: { uuid: 'yo' }});
     });
 
     model.put({id: 'test', name: 'test'}, (err, data) => {
+      expect(err).to.equal(null);
+      done();
+    });
+  });
+
+  it('should partialPut an item', function(done) {
+    var data = {
+      id: 'test',
+      name: 'test'
+    };
+
+    awsMock.mock('DynamoDB.DocumentClient', 'update', function (params, cb) {
+      expect(params).to.eql({
+        Key: {
+          HashKey: 'uuid',
+          NumberRangeKey: 'yo'
+        },
+        AttributeUpdates: {
+          name: { Action: 'PUT', Value: 'test' },
+          age: { Action: 'DELETE', Value: null }
+        },
+        ReturnValues: 'ALL_NEW'
+      });
+
+      cb(null, { Attributes: { uuid: 'yo' }});
+    });
+
+    model.partialPut('yo', data, ['name', 'age'], (err, data) => {
       expect(err).to.equal(null);
       done();
     });
@@ -50,21 +96,6 @@ describe('Adapter:Dynamodb', function() {
 
     model.del('yo', (err, data) => {
       expect(err).to.equal(null);
-      done();
-    });
-  });
-
-  it('should get an item', function(done) {
-    var item = { uuid: 'yo', key: 'value' };
-
-    awsMock.mock('DynamoDB.DocumentClient', 'get', function (params, cb){
-      expect(params.Key.uuid).to.equal('yo');
-      cb(null, { Item: item });
-    });
-
-    model.get('yo', (err, item) => {
-      expect(err).to.equal(null);
-      expect(item).to.equal(item);
       done();
     });
   });
